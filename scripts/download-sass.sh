d8f9944ff4a1f500cacd43358f2d4417a5e11ed8
kez <kez@torproject.org>
2022-12-20 12:15:00 -0800

    Add script to download dart sass

---
+++ scripts/download-sass.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# install the dart sass binary for x86_64 linux
+
+# change to the desired release tag and sha256 checksum
+# <https://github.com/sass/dart-sass/releases/>
+dart_release_tag=${dart_release_tag:-1.56.1}
+dart_tarball_checksum=${dart_tarball_checksum:-624f41102c59eb3eaf4a90726971a0604b42ccbe2265c13146f89d762ef1f74e}
+
+verify_checksum=${verify_checksum:-true}
+
+dart_tarball_filename=dart-sass-"$dart_release_tag"-linux-x64.tar.gz
+dart_tarball_url=https://github.com/sass/dart-sass/releases/download/"$dart_release_tag"/"$dart_tarball_filename"
+
+curl -s -O "$dart_tarball_filename" "$dart_tarball_url"
+
+if [ "$verify_checksum" != false ]; then
+    if ! sha256sum -c <(echo "$dart_tarball_checksum $dart_tarball_filename"); then
+        echo "bad checksum for $dart_tarball_filename"
+        echo "make sure you you set the right checksum in this script"
+        exit 1
+    fi
+else
+    echo "WARNING: verify_checksum set to false, not verifying the dart tarball"
+fi
+
+tar xf "$dart_tarball_filename" dart-sass/sass
+
+user_bin_dir="$(xdg-user-dir)"/.local/bin
+
+mkdir -p "$user_bin_dir"
+mv sass "$user_bin_dir"
+
+rm -r "$dart_tarball_filename" dart-sass
+
+echo "sass was successfully installed! make sure $user_bin_dir is in your PATH"
+echo "run this command to add it to your path now:"
+# shellcheck disable=SC2016
+echo 'echo PATH='"$user_bin_dir"':"$PATH" >> ' "$HOME/.bashrc && source $HOME/.bashrc"
