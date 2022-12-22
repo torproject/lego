0cf8b08a828720c3bd4ef2a86c30d7949dce90ef
kez <kez@torproject.org>
2022-12-22 09:34:51 -0800

    Update sass version

--- scripts/download-sass.sh
+++ scripts/download-sass.sh
@@ -6,8 +6,8 @@ set -e
 
 # change to the desired release tag and sha256 checksum
 # <https://github.com/sass/dart-sass/releases/>
-dart_release_tag=${dart_release_tag:-1.56.1}
-dart_tarball_checksum=${dart_tarball_checksum:-a5cf9e1f5db9456faffa440779a21a49cee65755c21617b75d1367d11448c59b}
+dart_release_tag=${dart_release_tag:-1.57.1}
+dart_tarball_checksum=${dart_tarball_checksum:-3bf345d4358c2908b6d05f8fcf239ddc412d076d71272d964371ba28e5f8fd9e}
 
 dart_tarball_filename=dart-sass-"$dart_release_tag"-linux-x64.tar.gz
 dart_tarball_url=https://github.com/sass/dart-sass/releases/download/"$dart_release_tag"/"$dart_tarball_filename"
